0eac41e273b4b1ac6d978d0d333fd47fadf75f3d
Anurekha-TechTeam <anurekhatechteam@gmail.com>
2025-06-10 16:42:27 +0530

    google login issue try 2

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -56,7 +56,7 @@ router.get(
         const cookieOptions = {
           httpOnly: true,
           secure: process.env.NODE_ENV === 'production',
-          sameSite: 'lax',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // Updated sameSite
           maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         };
 
